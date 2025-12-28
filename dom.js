765c338e040d5c18cdb2548c5a813d84f3d0be8c
kaziErfan <erfanhtamim1@gmail.com>
2025-12-29 02:01:05 +0600

    getAttribute & setAttribute example

--- dom.js
+++ dom.js
@@ -2,3 +2,8 @@
 // Changing href with setAttribute
 let a = document.querySelector("a");
 a.setAttribute("href", "https://www.google.com");
+
+// getAttribute
+console.log(a.getAttribute("href"));
+// removeAttribute
+console.log(a.removeAttribute("href"));
