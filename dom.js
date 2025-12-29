d87898958e7ea1458fe66cc1788b9a19cd5e0fbc
kaziErfan <erfanhtamim1@gmail.com>
2025-12-30 00:08:31 +0600

    changing css style using js

--- dom.js
+++ dom.js
@@ -13,3 +13,11 @@ console.log(a.removeAttribute("href"));
 let h1 = document.createElement("h1");
 h1.textContent = "Hello";
 document.body.append(h1);
+
+// Changing CSS style with JS
+h1.style.color = "green";
+h1.style.backgroundColor = "Yellow";
+h1.style.textAlign = "center";
+h1.style.padding = "30px";
+
+
