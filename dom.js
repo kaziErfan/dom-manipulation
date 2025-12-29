b66793e726f7d50daa14b7efb52d38b18ae3ecf0
kaziErfan <erfanhtamim1@gmail.com>
2025-12-29 23:44:06 +0600

    create & append element example

--- dom.js
+++ dom.js
@@ -7,3 +7,9 @@ a.setAttribute("href", "https://www.google.com");
 console.log(a.getAttribute("href"));
 // removeAttribute
 console.log(a.removeAttribute("href"));
+
+// Create & append element
+
+let h1 = document.createElement("h1");
+h1.textContent = "Hello";
+document.body.append(h1);
