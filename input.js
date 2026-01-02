ae5764cec3277f84f410e7c443840cb02aefa2b6
kaziErfan <erfanhtamim1@gmail.com>
2026-01-03 00:43:22 +0600

    get the uploaded file name in console

--- input.js
+++ input.js
@@ -4,3 +4,7 @@ let fileBasic = document.querySelector("#fileBasic");
 btn.addEventListener("click", function(){
     fileBasic.click();
 });
+
+fileBasic.addEventListener("change", function(dets){
+    console.log(dets.target.files[0].name);
+});
