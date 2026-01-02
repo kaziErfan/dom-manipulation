5bf34794626355d5056b698a309ce76b14decedf
kaziErfan <erfanhtamim1@gmail.com>
2026-01-03 00:54:52 +0600

    fixing an error which was happening when 2nd file upload cancel did

--- input.js
+++ input.js
@@ -1,10 +1,13 @@
 let btn = document.querySelector("#upload-btn");
 let fileBasic = document.querySelector("#fileBasic");
 
-btn.addEventListener("click", function(){
-    fileBasic.click();
+btn.addEventListener("click", function () {
+  fileBasic.click();
 });
 
-fileBasic.addEventListener("change", function(dets){
-   btn.textContent = dets.target.files[0].name;
+fileBasic.addEventListener("change", function (dets) {
+  const file = dets.target.files[0];
+  if (file) {
+    btn.textContent = file.name;
+  }
 });
