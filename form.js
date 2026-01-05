11dede19332c7e3933a2f95ef36db5bd5f69a33e
kaziErfan <erfanhtamim1@gmail.com>
2026-01-05 23:40:47 +0600

    getting input info in console

--- form.js
+++ form.js
@@ -1,5 +1,12 @@
 let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", function(dets){
-    console.log(dets)
+    dets.preventDefault();
+    console.log(
+        inputs[0].value,
+        inputs[1].value,
+        inputs[2].value,
+        inputs[3].value
+    );
 });
