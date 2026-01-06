0515d0316fb956ddc01a82cfe07f80eb3613d091
kaziErfan <erfanhtamim1@gmail.com>
2026-01-07 00:29:57 +0600

    creating a card when the form is submitted

--- form.js
+++ form.js
@@ -12,16 +12,16 @@ form.addEventListener("submit", function(dets){
     profile.classList.add("profile");
 
     let img = document.createElement("img");
-    img.setAttribute("src", "https://plus.unsplash.com/premium_photo-1664369473114-9146ca35bd72?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    img.setAttribute("src", inputs[0].value);
 
     let h3 = document.createElement("h3");
-    h3.textContent = "Erfan";
+    h3.textContent = inputs[1].value;
 
     let h5 = document.createElement("h5");
-    h5.textContent = "Developer";
+    h5.textContent = inputs[2].value;
 
     let p = document.createElement("p");
-    p.textContent = "This is a practice project";
+    p.textContent = inputs[3].value;
 
     profile.append(img);
     card.appendChild(profile);
