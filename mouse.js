3a4b1b7d308fd01fb0007834a4befdbd22560d8d
kaziErfan <erfanhtamim1@gmail.com>
2026-01-07 22:47:45 +0600

    use mousemove and create a moving element

--- mouse.js
+++ mouse.js
@@ -1,8 +1,14 @@
 let abcd = document.querySelector("#abcd");
+
 abcd.addEventListener("mouseover", function(){
     abcd.style.backgroundColor = "yellow";
 });
 
 abcd.addEventListener("mouseout", function(){
     abcd.style.backgroundColor = "red";
+});
+
+window.addEventListener("mousemove", function(fnc){
+   abcd.style.top = fnc.clientY + "px";
+   abcd.style.left = fnc.clientX + "px";
 });
