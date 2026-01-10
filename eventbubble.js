b6daf4800eabd290b131805152c24c727fe2074e
kaziErfan <erfanhtamim1@gmail.com>
2026-01-11 00:59:53 +0600

    created a input count box

--- eventbubble.js
+++ eventbubble.js
@@ -2,3 +2,11 @@ let ul = document.querySelector("ul");
 ul.addEventListener("click", function(dets){
     dets.target.classList.toggle("lt");
 });
+
+
+let inp = document.querySelector("input");
+let span = document.querySelector("span");
+
+inp.addEventListener("input", function(){
+   span.textContent = inp.value.length;
+})
