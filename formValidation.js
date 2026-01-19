e26d9a616a3dbe234e3cba358982c263f9213a3f
kaziErfan <erfanhtamim1@gmail.com>
2026-01-19 23:46:50 +0600

    created a regex for form validation in formValidation.js

--- formValidation.js
+++ formValidation.js
@@ -3,11 +3,15 @@ let form = document.querySelector("form");
 
 form.addEventListener("submit", function(evt){
     evt.preventDefault();
-
-    if(nm.value.length <= 2){
+    const regex = /^[a-zA-Z][a-zA-Z0-9_]{5,29}$/;
+    let ans = regex.test("a@a.a");
+    console.log(ans);
+    
+      if(nm.value.length <= 2){
         document.querySelector("#hide").style.display = "initial";
     }
     else{
         document.querySelector("#hide").style.display = "none";
     }
+  
 })
