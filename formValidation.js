5ff714dcbc57134b7d977d590e8da266a5b8f15a
kaziErfan <erfanhtamim1@gmail.com>
2026-01-23 01:08:40 +0600

    created a form for email and password validation

--- formValidation.js
+++ formValidation.js
@@ -3,15 +3,15 @@ let form = document.querySelector("form");
 
 form.addEventListener("submit", function(evt){
     evt.preventDefault();
-    const regex = /^[a-zA-Z][a-zA-Z0-9_]{5,29}$/;
-    let ans = regex.test("a@a.a");
+    const regex = /^[a-zA-Z0-9_]{3,20}$/;
+    let ans = regex.test("aa_a");
     console.log(ans);
-    
-      if(nm.value.length <= 2){
+
+   
+   if(nm.value.length <= 2){
         document.querySelector("#hide").style.display = "initial";
     }
     else{
         document.querySelector("#hide").style.display = "none";
     }
-  
 })
