052c7c03646221e51d52acdb409e065fae9ab8a7
kaziErfan <erfanhtamim1@gmail.com>
2026-01-23 01:18:56 +0600

    email and password regex added for validation

---
+++ emailPassValidator.js
@@ -0,0 +1,12 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])\S{8,}$/;
+
+    let emailAns = emailRegex.test(email.value);
+    let passwordAns = emailRegex.test(email.value);
+});
