36f8d2a48c21712613360f08e4ce9b8675351048
kaziErfan <erfanhtamim1@gmail.com>
2026-01-28 01:51:30 +0600

    added functionality to download progress bar with js

--- download.js
+++ download.js
@@ -2,12 +2,16 @@ let count = 0;
 let progress = document.querySelector(".progressFill");
 let progressText = document.querySelector("#progressPercent");
 
-setInterval(function(){
+let intVal = setInterval(function(){
     if(count<=99){
         count++;
         progress.style.width = `${count}%`;
         progressText.textContent = `${count}%`;
     }
-    
+    else{
+        document.querySelector("#statusText").textContent = "Downloaded";
+        clearInterval(intVal);
+
+    }
 
 }, (5 * 1000) / 100);
